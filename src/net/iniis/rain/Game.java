3796d423121a4863d2b33694aacc3b209f11b33a
INiiS <sebsattler@live.fr>
2018-01-15 15:04:55 +0100

    Added comments following episodes 1 to 3.

--- src/net/iniis/rain/Game.java
+++ src/net/iniis/rain/Game.java
@@ -3,13 +3,15 @@ package net.iniis.rain;
 import javax.swing.JFrame;
 import java.awt.*;
 
-/**
- * Projet tutorial Rain.
- * Tutorial original "Game Programming" par TheChernoproject : https://www.youtube.com/playlist?list=PLlrATfBNZ98eOOCk2fOFg7Qg5yoQfFAdf
+/*
+    Runnable makes sure the thread contains the game object.
  */
-
-
 public class Game extends Canvas implements Runnable {
+
+    /*
+    Defines the resolution of the game, with a scale so it can be scaled up in the future.
+    The height is defined relative to the width so that the ratio can be changed (here 16:9).
+     */
     public static int width = 300;
     public static int height = width / 16 * 9;
     public static int scale = 3;
@@ -25,12 +27,17 @@ public class Game extends Canvas implements Runnable {
         frame = new JFrame();
     }
 
+    /*
+    synchronized keyword ensures there is no overlaps of instructions for this thread.
+    An overlap could cause crashes or lockdowns.
+     */
     public synchronized void start() {
         running = true;
         thread = new Thread(this, "Display");
         thread.start();
 
     }
+
 
     public synchronized void stop() {
         running = false;
@@ -42,6 +49,11 @@ public class Game extends Canvas implements Runnable {
     }
 
     @Override
+    /*
+    Implementation of Runnable.run()
+    Main "game loop", so that our game runs after the 1st frame.
+    Without this loop, the game would stop after the 1st frame would have been rendered.
+     */
     public void run() {
 
         while (running) {
